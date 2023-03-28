98f1e7ec35f7ec7c5d9487097e92832aa80fe4d0
vivek tripathi <sopan.tripathi@gmail.com>
2023-03-28 20:57:09 +0530

    Added Sequence Unpack

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # har-har-mahadev
 This repository is to advance my python skills.
+
+- Unpacking Sequence
+    
+        Demonstration of unpacking of a Sequence (tuple, list). 
---
+++ pehla-chap/unpack_seq.py
@@ -0,0 +1,15 @@
+p = (1,2,3)
+
+# unpacking using variables
+[x, y, z] = p
+print(x, y, z)
+
+# unpacking specific value is done by using index
+a = p[1]
+print (a)
+
+# printing 
+size_of_p = len(p)
+print("Size of p is " + str(size_of_p )) # print by concatenation
+print(f"New Size of p is {size_of_p}") # print by f-value
+print("New New Size of p is", size_of_p ) # print by comma method
